3269bf583ceaf43afeb530ce8273d26eb385b54c
Adam Bien <abien@adam-bien.com>
2018-03-07 11:10:04 +0100

    Caused by attendee questions -> extreme solution

--- marketing/src/main/java/com/airhacks/marketing/vacations/boundary/FlightsCatalog.java
+++ marketing/src/main/java/com/airhacks/marketing/vacations/boundary/FlightsCatalog.java
@@ -1,14 +1,17 @@
 
 package com.airhacks.marketing.vacations.boundary;
 
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.json.JsonArray;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 /**
  *
@@ -20,15 +23,25 @@ public class FlightsCatalog {
     private Client client;
     private WebTarget tut;
 
+    @Resource
+    ManagedExecutorService mes;
+
     @PostConstruct
     public void init() {
-        this.client = ClientBuilder.newClient();
-        this.tut = this.client.target("http://localhost:8080/vacs/resources/flights");
+        this.client = ClientBuilder.newBuilder().
+                connectTimeout(1, TimeUnit.SECONDS).
+                readTimeout(1, TimeUnit.SECONDS).
+                executorService(mes).
+                build();
+        this.tut = this.client.
+                target("http://localhost:8080/vacs/resources/flights");
     }
 
-    public void all() {
-        Response getResponse = this.tut.request(MediaType.APPLICATION_JSON).get();
-        JsonArray flightsArray = getResponse.readEntity(JsonArray.class);
+    public CompletionStage<JsonArray> all() {
+        return this.tut.
+                request(MediaType.APPLICATION_JSON).
+                rx().
+                get(JsonArray.class);
     }
 
 }
--- marketing/src/main/java/com/airhacks/marketing/vacations/boundary/FlightsResource.java
+++ marketing/src/main/java/com/airhacks/marketing/vacations/boundary/FlightsResource.java
@@ -1,9 +1,13 @@
 
 package com.airhacks.marketing.vacations.boundary;
 
+import java.util.concurrent.TimeUnit;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
 /**
  *
@@ -15,5 +19,14 @@ public class FlightsResource {
 
     @Inject
     FlightsCatalog catalog;
+
+    @GET
+    public void flights(@Suspended AsyncResponse response) {
+        response.setTimeout(2, TimeUnit.SECONDS);
+
+        this.catalog.all().thenAccept(response::resume);
+
+    }
+
 
 }
