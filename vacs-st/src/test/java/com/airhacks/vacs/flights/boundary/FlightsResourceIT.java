4332eae3a2351fe0409526fd1caf4ee8353ee7be
Adam Bien <abien@adam-bien.com>
2018-03-07 12:12:08 +0100

    empty and big object STs written

--- vacs-st/src/test/java/com/airhacks/vacs/flights/boundary/FlightsResourceIT.java
+++ vacs-st/src/test/java/com/airhacks/vacs/flights/boundary/FlightsResourceIT.java
@@ -3,6 +3,7 @@
 package com.airhacks.vacs.flights.boundary;
 
 import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -10,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,6 +38,23 @@ public class FlightsResourceIT {
         JsonArray flightsArray = getResponse.readEntity(JsonArray.class);
         assertFalse(flightsArray.isEmpty());
         System.out.println("flightsArray = " + flightsArray);
+    }
+
+    @Test
+    public void empty() {
+        Response emptyResponse = this.tut.path("7").request(MediaType.APPLICATION_JSON).get();
+        assertThat(emptyResponse.getStatus(), is(204));
+    }
+
+    @Test
+    public void bigObject() {
+        Response bigResponse = this.tut.path("42").request(MediaType.APPLICATION_JSON).get();
+        assertThat(bigResponse.getStatus(), is(200));
+        JsonObject bigObject = bigResponse.readEntity(JsonObject.class);
+        assertNotNull(bigObject.getJsonObject("flight"));
+        assertNotNull(bigObject.getString("metadata"));
+
+        System.out.println("bigObject = " + bigObject);
     }
 
 }
