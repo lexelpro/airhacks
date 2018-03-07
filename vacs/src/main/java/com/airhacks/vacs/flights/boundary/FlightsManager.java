76b960905995396804056af504a6d2c014d686a5
Adam Bien <abien@adam-bien.com>
2018-03-07 12:04:52 +0100

    Optional makes objects bigger

--- vacs/src/main/java/com/airhacks/vacs/flights/boundary/FlightsManager.java
+++ vacs/src/main/java/com/airhacks/vacs/flights/boundary/FlightsManager.java
@@ -4,6 +4,7 @@ package com.airhacks.vacs.flights.boundary;
 import com.airhacks.vacs.flights.entity.Flight;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import javax.ejb.Stateless;
 
 /**
@@ -15,6 +16,13 @@ public class FlightsManager {
 
     public List<Flight> all() {
         return Arrays.asList(new Flight("lh42"), new Flight("lh-4"));
+    }
+
+    public Optional<Flight> getFlight(String number) {
+        if (number == null || number.contains("7")) {
+            return Optional.empty();
+        }
+        return Optional.of(new Flight(number));
     }
 
 
--- vacs/src/main/java/com/airhacks/vacs/flights/boundary/FlightsResource.java
+++ vacs/src/main/java/com/airhacks/vacs/flights/boundary/FlightsResource.java
@@ -7,8 +7,11 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 @Stateless
 @Path("flights")
@@ -25,6 +28,32 @@ public class FlightsResource {
                 map(Flight::toJSON).
                 forEach(retVal::add);
         return retVal.build();
+    }
+
+    @GET
+    @Path("{nbr}")
+    public Response flight(@PathParam("nbr") String number) {
+        return this.fm.getFlight(number).
+                map(Flight::toJSON).
+                map(this::makeBiggerObject).
+                map(this::convert).
+                orElseGet(this::empty);
+    }
+
+    Response empty() {
+        return Response.status(204).header("info", "no vacations for you").build();
+    }
+
+    Response convert(JsonObject input) {
+        return Response.status(200).
+                entity(input).
+                build();
+    }
+
+    JsonObject makeBiggerObject(JsonObject input) {
+        return Json.createObjectBuilder(input).
+                add("first", "a little bigger").
+                build();
     }
 
 
